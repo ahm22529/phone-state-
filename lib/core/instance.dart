9105f48cb1018a2b871015aa985fffd2307ee829
ahm22529 <ahmedehdeeb@gmail.com>
2024-06-23 17:48:59 +0300

    c

--- lib/core/instance.dart
+++ lib/core/instance.dart
@@ -1,4 +1,3 @@
-
 import 'package:get_it/get_it.dart';
 import 'package:silent/features/homeofapp/data/contactrepimp.dart';
 import 'package:silent/features/loginscreen/data/Loginrepo.dart';
@@ -6,9 +5,9 @@ import 'package:silent/features/loginscreen/data/loginimp.dart';
 import 'package:silent/features/loginscreen/data/repoimp.dart';
 
 final getIt = GetIt.instance;
-void setup()
-{
+void setup() {
   getIt.registerSingleton<Singuprepoimp>(Singuprepoimp());
-   getIt.registerSingleton<Loginrepo>(Loginimp());
-      getIt.registerSingleton<contactrepoim>(contactrepoim());
+  getIt.registerSingleton<Loginrepo>(Loginimp());
+  getIt.registerSingleton<contactrepoim>(contactrepoim());
+  print("object");
 }
